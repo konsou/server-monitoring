24b3f86d4134b274dc9ae51d701d8ca3fc39003a
konso <tomi.javanainen@proton.me>
2025-10-20 16:37:34 +0300

    Add test for short

--- tests/test__smart.py
+++ tests/test__smart.py
@@ -105,3 +105,86 @@ class TestSmart(TestCase):
             [call("/dev/sda"), call("/dev/sdb"), call("/dev/sdc")],
             msg="status() should be called with correct device names",
         )
+
+    @patch("_smart.poll_time")
+    @patch("_smart.device_info")
+    @patch("subprocess.run")
+    def test_short_calls_smartctl(self, mock_run, mock_device_info, mock_poll_time):
+        disable_logging()
+        mock_device_info.return_value = INFO_DICT_STATUS_OK
+        mock_poll_time.return_value = 0
+        _smart.short("/dev/dummy")
+        mock_run.assert_called_with(
+            ["smartctl", "--test=short", "--json", "/dev/dummy"]
+        )
+
+    @patch("_smart.poll_time")
+    @patch("_smart.human_readable_error_info")
+    @patch("_smart.device_info")
+    @patch("subprocess.run")
+    def test_short_succeeds_when_drive_ok(
+        self, _, mock_device_info, mock_human_readable_error_info, mock_poll_time
+    ):
+        disable_logging()
+        mock_device_info.return_value = INFO_DICT_STATUS_OK
+        mock_human_readable_error_info.return_value = ""
+        mock_poll_time.return_value = 0
+        res = _smart.short("/dev/dummy")
+        self.assertEqual(
+            res,
+            DeviceTestResult(
+                device="/dev/dummy",
+                human_readable_name="SAMSUNG MZYLF128HCHP-000L2 - 119.2GB",
+                passed=True,
+                test_type=_smart_types.TestType.SHORT,
+                human_readable_error_info="",
+            ),
+        )
+
+    @patch("_smart.test_passed")
+    @patch("_smart.poll_time")
+    @patch("_smart.human_readable_error_info")
+    @patch("_smart.device_info")
+    @patch("subprocess.run")
+    def test_short_fails_when_drive_not_ok(
+        self,
+        _,
+        mock_device_info,
+        mock_human_readable_error_info,
+        mock_poll_time,
+        mock_test_passed,
+    ):
+        disable_logging()
+        mock_device_info.return_value = INFO_DICT_STATUS_FAIL
+        mock_human_readable_error_info.return_value = "Mock error info"
+        mock_poll_time.return_value = 0
+        mock_test_passed.return_value = False
+        res = _smart.short("/dev/dummy")
+        self.assertEqual(
+            DeviceTestResult(
+                device="/dev/dummy",
+                human_readable_name="SAMSUNG MZYLF128HCHP-000L2 - 119.2GB",
+                passed=False,
+                test_type=_smart_types.TestType.SHORT,
+                human_readable_error_info="Mock error info",
+            ),
+            res,
+        )
+
+    @patch("_smart.short")
+    @patch("_smart.scan_devices")
+    def test_short_all_calls_short(self, mock_scan, mock_short):
+        disable_logging()
+        mock_scan.return_value = ("/dev/sda", "/dev/sdb", "/dev/sdc")
+
+        _smart.short_all()
+        self.assertEqual(
+            len(mock_short.call_args_list),
+            3,
+            msg="short() should be called for every device",
+        )
+        self.assertEqual(
+            mock_short.call_args_list,
+            [call("/dev/sda"), call("/dev/sdb"), call("/dev/sdc")],
+            msg="short() should be called with correct device names",
+        )
