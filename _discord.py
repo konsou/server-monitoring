34a7cb2076382c672bb1b2ceea9d72db7b77e2ef
konso <tomi.javanainen@proton.me>
2026-04-09 21:35:51 +0300

    Use logger when outputting Discord notify results

--- _discord.py
+++ _discord.py
@@ -9,7 +9,11 @@ def notify_discord(message: str, is_error: bool = False):
     notify_args = ["notify-discord", message]
     if is_error:
         notify_args.extend(["--level", "error"])
-    subprocess.run(notify_args)
+    notify_result = subprocess.run(notify_args, capture_output=True, text=True)
+    if notify_result.returncode == 0:
+        logger.info(notify_result.stdout)
+    else:
+        logger.error(notify_result.stdout)
 
 
 def log_and_notify_discord(message: str, is_error: bool = False):
