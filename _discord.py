98cee3823db6e19c81f2abbdab729e1b5482796d
konso <tomi.javanainen@proton.me>
2026-04-09 21:39:34 +0300

    Add error handling for when notify-discord is not available

--- _discord.py
+++ _discord.py
@@ -9,7 +9,15 @@ def notify_discord(message: str, is_error: bool = False):
     notify_args = ["notify-discord", message]
     if is_error:
         notify_args.extend(["--level", "error"])
-    notify_result = subprocess.run(notify_args, capture_output=True, text=True)
+
+    try:
+        notify_result = subprocess.run(notify_args, capture_output=True, text=True)
+    except FileNotFoundError:
+        logger.error(
+            f"notify-discord executable not found in path, can't send discord notifications"
+        )
+        return
+
     if notify_result.returncode == 0:
         logger.info(notify_result.stdout)
     else:
